606f94ed1bbf06f8c2ecd6239239272c34af117c
JashanRalla <jashanpreet.ralla@ada.ac.uk>
2025-08-26 00:59:53 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,21 @@
-# entrypoint for Gunicorn on Azure
-from app import app           
-application = app              
+app = None
+
+try:
+    from app import app as _app
+    app = _app
+except Exception:
+    try:
+        from application import app as _app
+        app = _app
+    except Exception:
+        try:
+            from application import application as _app
+            app = _app
+        except Exception:
+            from application import create_app
+            app = create_app()
+
+application = app
 
 if __name__ == "__main__":
     app.run()
